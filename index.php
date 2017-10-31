a514dbd76110345f7a22161a640ecdfc77edaa0b
shawonis08 <shawon_math07@yahoo.com>
2017-10-31 13:27:55 +0600

    3

--- index.php
+++ index.php
@@ -1,7 +1,23 @@
-<!DOCTYPE html>
+<?php
+/**
+ * Created by PhpStorm.
+ * User: shawo
+ * Date: 10/31/2017
+ * Time: 12:42 PM
+ */
+
+if ($_REQUEST){
+    $user=$_REQUEST['txtusername'];
+    $pass=$_REQUEST['txtpassword'];
+
+    print_r($user,$pass);
+
+}
+
+?>
+
 <html>
 <head>
-
 	<title>Login Panel</title>
 	<!--CSS-->
 	<link rel="stylesheet" type="text/css" href="css/style.css">
@@ -10,15 +26,15 @@
 	<div class="container">
 		<div class="container-body">
 			
-			<form class="form">
+			<form class="form" method="post" action="test.php">
 				<div class="header">
 					<p>Admin Panel</p>
 				</div>
 				<label for="username">Username</label>
-				<input type="text" name="" id="username" placeholder="Username">
+				<input type="text" name="txtusername" id="username" placeholder="Username">
 				<br>
 				<label for="password">Password&nbsp</label>
-				<input type="password" name="" id="password" placeholder="Password">
+				<input type="password" name="txtpassword" id="password" placeholder="Password">
 				<br>
 				<input type="checkbox">
 				<span>Remember me</span>
